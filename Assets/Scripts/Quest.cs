f470b11f084bd3342e8027b557625a89620f69f9
Anderson Contreras <alexandearko@gmail.com>
2019-08-01 10:43:28 -0400

    Añadido el Script de QuestManager y Quest

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour {
+
+    public int questID;
+    private QuestManager manager;
+
+    public string startText, completedText;
+	// Use this for initialization
+	void Start () {
+
+        manager = FindObjectOfType<QuestManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartQuest()
+    {
+        manager.ShowQuestText(startText);
+    }
+
+    public void CompletedQuest()
+    {
+        manager.ShowQuestText(completedText);
+        manager.questCompleted[questID] = true;
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour {
+
+    public Quest[] quest;
+    public bool[] questCompleted;
+
+    private DialogManager manager;
+
+	// Use this for initialization
+	void Start () {
+
+        questCompleted = new bool[quest.Length];
+        manager = FindObjectOfType<DialogManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowQuestText(string questText)
+    {
+        string[] dialogLines = new string[]
+        {
+            questText
+        };
+        manager.ShowDialog(dialogLines);
+    }
+}
