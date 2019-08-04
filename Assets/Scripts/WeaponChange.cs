0a77c712b3181b17cc9e7a354d0d6926fe388c22
Anderson Contreras <alexandearko@gmail.com>
2019-08-04 19:06:29 -0400

    Añadido Script WeaponManager, el cual permite cambiar de arma. Añadido un Staff como arma. Presionar 1 o 2 para cambair armas

---
+++ Assets/Scripts/WeaponChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponChange : MonoBehaviour {
+
+    private WeaponManager manager;
+    public int weaponID;
+    public bool equipOnStart;
+	// Use this for initialization
+	void Start () {
+        manager = FindObjectOfType<WeaponManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour {
 
+    public Weapon[] weapon;
+    public int currentWeapon;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeWeapon(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeWeapon(1);
+        }
 	}
-}
+    public void ChangeWeapon(int newWeapon)
+    {
+        weapon[currentWeapon].gameObject.SetActive(false);
+        currentWeapon = newWeapon;
+        weapon[currentWeapon].gameObject.SetActive(true);
+        Debug.Log(weapon[currentWeapon]);
+    }
+
+}   
